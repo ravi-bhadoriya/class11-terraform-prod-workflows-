e8634bc8a7a3922c65df9b745ac8e7ca45cc48aa
Ravi pratap singh bhadoriya <ravipratapsinghbhadoriya@Ravis-MacBook-Pro.local>
2026-05-17 14:55:56 +0530

    test tfsec

---
+++ modules/security/main.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "ec2_sg" {
+  name   = "ec2-sg"
+  vpc_id = aws_vpc.main.id
+
+  # BAD RULE (intentional for tfsec/demo)
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
